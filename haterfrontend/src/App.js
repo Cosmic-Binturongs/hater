af8d3ca4a6aa5f4a868154f9e8d96c8b42c0435f
David White-Goode <davidwhite-goode@Davids-MacBook-Air.local>
2022-05-03 15:28:13 -0400

    moved seed.sql file

--- haterfrontend/src/App.js
+++ haterfrontend/src/App.js
@@ -1,30 +1,11 @@
 import Landing from "./screens/Landing.jsx";
 import Home from "./screens/Home.jsx";
-
-import Profile from "./components/profile";
-
 import ErrorPage from "./screens/Error";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-
 function App() {
   return (
-<>
-    <div className="fullDiv">
-      <Profilebutton click={handleClick} />
-      <Profiletab
-        name={"verybamboo"}
-        tweets={114}
-        likes={426}
-        click={closeBox}
-        show={show}
-      />
-      <div className="mid">
-        <Profile />
-      </div>
-      <div className="right">3</div>
-    </div>
     <>
       <Routes>
         <Route path="/" element={<Landing></Landing>}></Route>
@@ -32,8 +13,6 @@ function App() {
         <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
       </Routes>
     </>
-                                 </>
-
   );
 }
 
--- haterfrontend/src/components/profile.jsx
+++ haterfrontend/src/components/profile.jsx
@@ -11,7 +11,7 @@ export default function Profile() {
     <div className='mid'>
       <div className="pro-container grid-container">
         <div className="headerP">
-          <h4>David</h4>
+          <h4 className="profile-h4">David</h4>
           <p>191 Tweets</p>
         </div>
         <div className="bkgrndP">
@@ -22,12 +22,12 @@ export default function Profile() {
           <input type="button" onClick={"handleClick"} className="button-62" name="" value="Edit profile" id="" />
         </div>
         <div className='userNames'>
-          <h4>David</h4>
+          <h4 className="profile-h4">David</h4>
           <p>@davewgoode</p>
         </div>
         <div className="profileInfo">
 
-          <h4>Gigachad reaact developer and influencer. </h4>
+          <h4 className="profile-h4">Gigachad reaact developer and influencer. </h4>
         </div>
         <div className="user-locale-date">
           <p>Brooklyn, NY </p><p>Date Joined: 2009</p>
