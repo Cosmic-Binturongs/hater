78de7ad4adebb3dd84960b6a96ff58b76e366ce7
George Chen <georgechen1338@gmail.com>
2022-05-04 15:20:49 -0400

    gcwedhatesicons

--- haterfrontend/src/components/hates/Hate.jsx
+++ haterfrontend/src/components/hates/Hate.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import HeartBrokenIcon from '@mui/icons-material/HeartBroken';
+import MessageIcon from '@mui/icons-material/Message';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
 
 export default function Hate({ hateData, setToggle }) {
   const [hate, setHate] = useState({
@@ -17,14 +20,25 @@ export default function Hate({ hateData, setToggle }) {
           {hate.name} {hate.tag}
           </h3>
         <div className="hate-info">
-        <h2
-          className="hate-text"
-          type="text"
-          name="text">
-          {hate.text}
-        </h2>
+          <h2
+            className="hate-text"
+            type="text"
+            name="text">
+            {hate.text}
+          </h2>
+          <div className='hate-buttons'>
+            <button className='hate-criticisms' title="Criticism">
+              <MessageIcon className='hate-crit'></MessageIcon> 123
+            </button>
+            <button className='hate-rehate' title="Rehate">
+              <AutorenewIcon className='hate-renew'></AutorenewIcon> 456
+            </button>
+            <button className='hate-dislike' title="Dislike">
+              <HeartBrokenIcon></HeartBrokenIcon> 789
+            </button>
           </div>
         </div>
       </div>
+    </div>
   )
 }
