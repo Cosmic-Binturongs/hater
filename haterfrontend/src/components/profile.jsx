ad84cb170f8f62478850357e36857d362411e7e6
Lawrence Yee <lawrenceyee521@Lawrences-MacBook-Pro.local>
2022-05-04 17:14:04 -0400

    BIG QUERY FIXES

--- haterfrontend/src/components/profile.jsx
+++ haterfrontend/src/components/profile.jsx
@@ -8,11 +8,11 @@ export default function Profile(props) {
 
 
   return (
-    
+      
       <div className="pro-container grid-container">
         <div className="headerP">
           <h4 className="profile-h4">David</h4>
-          <p>191 Tweets</p>
+          <p classname="pro-tweet-count">191 Tweets</p>
         </div>
         <div className="bkgrndP">
         </div>
