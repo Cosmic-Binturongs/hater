3dfbcaaa3c1a165df2b2577ef24f5970658579e0
George Chen <georgechen1338@gmail.com>
2022-05-05 11:09:35 -0400

    latest working model

--- haterfrontend/src/components/hates/HatesFeed.jsx
+++ haterfrontend/src/components/hates/HatesFeed.jsx
@@ -45,8 +45,8 @@ export default function HatesFeed({ hates, setToggle }) {
   return (
     <div className="hates-list">
       {hates.length ?
-          hates.map((hate) => (
-            <Hate setToggle={setToggle} key={hate._id} hateData={hate} />
+          hates.map((hate,id) => (
+            <Hate setToggle={setToggle} key={id} hateData={hate} />
           ))
           :
           <Error />
