6bf86dbe9f4a900f40a68fb13c6e3f9695763d80
mustafabin <mbin828@gmail.com>
2022-05-05 15:37:59 -0400

    comment route done with url params

--- haterBackEnd/hater_app/serializers.py
+++ haterBackEnd/hater_app/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
-from .models import Criticism, User_profile, Hates, Dislike, Follower
+from .models import Criticism, User_profile, Hates
 from django.contrib.auth.models import User
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
 class User_profileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_profile
@@ -13,21 +16,27 @@ class HatesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hates
         fields = '__all__'
+        
 
-
-class DislikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Dislike
-        fields = '__all__'
-
-
-class FollowerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Follower
-        fields = '__all__'
-
-
-class CriticismProfileSerializer(serializers.ModelSerializer):
+class CriticismSerializer(serializers.ModelSerializer):
     class Meta:
         model = Criticism
         fields = '__all__'
+
+class CommentSerializer(serializers.BaseSerializer):
+    def to_representation(self, instance):
+      return {
+        'id': instance.id,
+        'comment': instance.c_body,
+        'hater_id': instance.hater.id,
+        'hater_id': instance.hater.name,
+        'hate_id': instance.hate.id,
+        'hate_content': instance.hate.h_body,
+      }
+class TestSerializer(serializers.BaseSerializer):
+    def to_representation(self, instance):
+      return {
+        'hate': instance.h_body,
+        'hater': instance.haters.name,
+        'tag': instance.haters.tag
+      }
