933e5515a7f3c58d4560d62aa556b4447fb77bc5
Lawrence Yee <lawrenceyee521@Lawrences-MacBook-Pro.local>
2022-05-05 10:11:15 -0400

    profile screen made

--- haterfrontend/src/App.js
+++ haterfrontend/src/App.js
@@ -1,20 +1,19 @@
 import Landing from "./screens/Landing.jsx";
 import Home from "./screens/Home.jsx";
+import ProfilePage from "./screens/ProfilePage.jsx";
 import ErrorPage from "./screens/Error";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing></Landing>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/profile" element={<ProfilePage></ProfilePage>}></Route>
         <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
       </Routes>
-
     </>
   );
 }
