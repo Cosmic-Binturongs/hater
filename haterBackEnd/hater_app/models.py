5a8d015a739bbc7932fc349205ff4a09ec681312
Evan Mei <em@Evans-MacBook-Pro.local>
2022-05-06 10:45:09 -0400

    added date time to hates

--- haterBackEnd/hater_app/models.py
+++ haterBackEnd/hater_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
 
 
@@ -9,20 +10,26 @@ class User_profile(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+
 class Hates(models.Model):
+
     haters = models.ForeignKey(User_profile, on_delete=models.CASCADE)
     h_body = models.CharField(max_length=120)
     hate_count = models.IntegerField()
     rehate_count = models.IntegerField()
     crit_count = models.IntegerField()
+    hate_date = models.DateTimeField(default=datetime.now())
+
     def __str__(self):
-      return self.h_body
-        
+        return self.h_body
+
+
 class Criticism(models.Model):
     hater = models.ForeignKey(
         User_profile, on_delete=models.CASCADE, default=4)
     c_body = models.CharField(max_length=140)
     hate = models.ForeignKey(Hates, on_delete=models.CASCADE, default=4)
+
     def __str__(self):
         return self.c_body
