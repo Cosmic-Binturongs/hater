de20c28285ab10a629c76842c54bd0010339d4a9
David White-Goode <davidwhite-goode@Davids-MacBook-Air.local>
2022-05-06 14:07:21 -0400

    more clarity

--- haterfrontend/src/textService.js
+++ haterfrontend/src/textService.js
@@ -6,13 +6,14 @@ let sendSMS = () => {
   const authToken = '6f09c307d1f608def52c74fca3eb23ac'
   const client = require('twilio')(accountSid, authToken);
 
+  const twilioVirtualNumber = '19894553207'
   const user_handle = ''
   const user_phone = ''
 
   client.messages
     .create({
       body: 'Your hate is getting a lot of negativity!',
-      from: `19894553207`,
+      from: twilioVirtualNumber,
       to: `16467093405`
     })
     .then(message => console.log(message.sid));
