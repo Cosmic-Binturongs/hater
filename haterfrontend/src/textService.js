7138411060150c7c7e2f402a12cd77418c9dfefe
David White-Goode <davidwhite-goode@Davids-MacBook-Air.local>
2022-05-06 14:30:03 -0400

    hiding the auth token and account number in environment variables for security

--- haterfrontend/src/textService.js
+++ haterfrontend/src/textService.js
@@ -2,8 +2,8 @@
 
 let sendSMS = () => {
 
-  const accountSid = 'AC341560422bf71c1a6fe53115b1067dd6';
-  const authToken = '6f09c307d1f608def52c74fca3eb23ac'
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require('twilio')(accountSid, authToken);
 
   const twilioVirtualNumber = '19894553207'
